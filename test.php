cea7d166835db92403907701d6ede128bedbea36
Hafiz Putra Ludyanto <fairyhunter13@gmail.com>
2018-04-11 17:24:23 +0700

    Fixing - Fixing dockerfile for ubuntu to add sqlserver extension :whale: - Delete image for alpine (not supported musl lib sqlserv extension) :fire: - Adding test file to check sqlserver connection :white_check_mark: - Adding xdebug to prepare debugging :heavy_plus_sign:

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+$serverName = "mssqlserver";
+$connectionOptions = [
+    // "Database" => "yourDatabase",
+    "Uid" => "sa",
+    "PWD" => "fairy@test13"
+];
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn) {
+     echo "Connection established.<br />";
+    $version = sqlsrv_query($conn, 'SELECT @@VERSION'); 
+    $row = sqlsrv_fetch_array($version);
+
+    echo $row[0];
+
+    // Clean up
+    sqlsrv_free_stmt($version);
+} else {
+     echo "Connection could not be established.<br />";
+     die(print_r( sqlsrv_errors(), true));
+}
