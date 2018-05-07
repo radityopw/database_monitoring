ddd4663101a1c15f85bc091107975c5525b9893a
Hafiz Putra Ludyanto <fairyhunter13@gmail.com>
2018-05-07 10:54:45 +0700

    Progress - Working on folder and program structure :construction: :art:

--- config/database.php
+++ config/database.php
@@ -1,20 +1,22 @@
 <?php
 
-require_once '';
-
 return [
     'connections' => [
         'sqlsrv' => [
-            'host' => env('SQLSRV_DB_HOST'),
-            'port' => env('SQLSRV_DB_PORT'),
-            'database' =>   env('SQLSRV_DB_DATABASE'),
-            'username' => env('SQLSRV_DB_USERNAME'),
+            'host' => env('SQLSRV_DB_HOST',''),
+            'port' => env('SQLSRV_DB_PORT',''),
+            'database' =>   env('SQLSRV_DB_DATABASE',''),
+            'username' => env('SQLSRV_DB_USERNAME',''),
             'password' => env('SQLSRV_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '', 
         ],
         'neo4j' => [
-
+            'host' => env('NEO4J_DB_HOST',''),
+            'port' => env('NEO4J_DB_PORT',''),
+            'database' =>   env('NEO4J_DB_DATABASE',''),
+            'username' => env('NEO4J_DB_USERNAME',''),
+            'password' => env('NEO4J_DB_PASSWORD', ''),
         ]
     ]
 ];
