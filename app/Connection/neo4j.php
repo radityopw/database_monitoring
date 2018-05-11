12cf2597fc51fe2928362f480043a6ff2a3f5ef6
Unknown <fairyhunter13@gmail.com>
2018-05-11 17:06:19 +0700

    Progress
    - Working on Connection in Database :construction:

--- app/Connection/neo4j.php
+++ app/Connection/neo4j.php
@@ -2,3 +2,8 @@
 
 namespace UserDep\Connection;
 
+use GraphAware\Neo4j\Client\ClientBuilder;
+
+return ClientBuilder::create()
+        ->addConnection('bolt', 'bolt://neo4j:password@localhost:7687')
+        ->build();;
--- app/extract.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace UserDep;
-
-require_once __DIR__.'/Index.php';
-$sqlsrvConn = require_once __DIR__.'/Connection/sqlsrv.php';
