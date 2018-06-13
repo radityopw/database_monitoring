6aef86f22fd6b4a68a0ce5cbc9e723dc3b5c962c
Hafiz Putra Ludyanto <fairyhunter13@gmail.com>
2018-06-14 04:54:01 +0700

    Progress -Adding database config syntax :wrench: - Ignoring database config :see_no_evil:

---
+++ config/database_config_syntax.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'connections' => [
+        'sqlsrv' => [
+            'host' => '',
+            'port' => 0,
+            'database' =>   '',
+            'username' => '',
+            'password' => '',
+            'charset' => '',
+            'prefix' => '', 
+        ],
+        'neo4j' => [
+            'sp' => [
+                'host' => '',
+                'port' => 0,
+                'username' => '',
+                'password' => '',
+            ],
+            'user' => [
+                'host' => '',
+                'port' => 0,
+                'username' => '',
+                'password' => '',
+            ],
+            'username_read' => '',
+            'password_read' => '',
+        ]
+    ]
+];
