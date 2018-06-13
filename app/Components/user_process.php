6c310270033bc1d6d54e4266b8beb4cbd2dd28b4
Hafiz Putra Ludyanto <fairyhunter13@gmail.com>
2018-06-13 18:28:22 +0700

    Progress - Adding indexes to processor component :sparkles:

--- app/Components/user_process.php
+++ app/Components/user_process.php
@@ -2,27 +2,42 @@
 
 namespace Dependency\Components;
 
+/**
+ * Get all vendor and required components.
+ */
 require_once __DIR__.'/../hihi.php';
 
+/**
+ * Get all config from php file.
+ * Return value of array in the php file.
+ */
 $databaseConfig = require config_path('database.php');
 $queryConfig = require config_path('query.php');
 $extractResult = require __DIR__.'/user_extract.php';
 // dd($extractResult);
 
-//Config
+/**
+ * Extract config from php file array.
+ */
 $neo4jConfig = $databaseConfig['connections']['neo4j']['user'];
 $neo4jAllConfig = $databaseConfig['connections']['neo4j'];
 $sqlSrvConfig = $databaseConfig['connections']['sqlsrv'];
 // dd($sqlSrvConfig);
 
-//Connection
+/**
+ * Create all connection, neo4j and sql server.
+ */
 $neo4j = createNeo4jConnection($neo4jConfig['username'], $neo4jConfig['password'], $neo4jConfig['host'], $neo4jConfig['port']);
 $sqlsrv = createSqlServerConnection($sqlSrvConfig['host'], $sqlSrvConfig['port'], $sqlSrvConfig['username'], $sqlSrvConfig['password']);
 
-//Query
+/**
+ * Query to get servername from sql server.
+ */
 $extractServer  = $queryConfig['sqlserver']['extract_servername'];
 
-//Get servername from sql server
+/**
+ * Get servername from sql server with query.
+ */
 $serverName = tap($sqlsrv->prepare($extractServer))
 ->execute()
 ->fetchObject()
@@ -31,13 +46,21 @@ $serverName = tap($sqlsrv->prepare($extractServer))
 $serverArray  = [
     'serverName' => $serverName
 ];
-//Delete all nodes and relationships
+/**
+ * Delete all nodes and relationships.
+ * Rebuild all of the graph.
+ */
 $stack = tap($neo4j->stack())->push("MATCH (n) DETACH DELETE n");
 
-//User and password for user with role reader
+/**
+ * User and Password for user with role reader
+ */
 $user = $neo4jAllConfig['username_read'];
 $password = $neo4jAllConfig['password_read'];
-//Create user with role Reader
+/**
+ * Create user with role Reader.
+ * Delete the user if user already exists.
+ */
 try {
     $neo4j->run("
     CALL dbms.security.deleteUser('$user')
@@ -52,8 +75,11 @@ try {
     MATCH (n)
     RETURN n");
 }
-//Create new node from server
+/**
+ * Create a new node server with index on name and surname
+ */
 $stack->push("MERGE (s:Server {name: {serverName}, surname: {serverName}})", $serverArray);
+$stack->push("CREATE INDEX ON :Server(name, surname)");
 $serverPrefix = "{$serverName}.";
 
 foreach ($extractResult as $result) {
@@ -64,11 +90,14 @@ foreach ($extractResult as $result) {
         'databaseSurname' => rtrim($databasePrefix, "."),
     ];
     $serverToDbArray =  array_merge($serverArray, $databaseArray);
-    //Create Server to Database Relationships
+    /**
+     * Create Server to Database relationship with creating new index for name and surname on Database label
+     */
     $stack->push("MERGE (s:Server {name: {serverName}, surname: {serverName}}) 
         MERGE (d:Database {name: {databaseName}, surname: {databaseSurname}}) 
         MERGE (s)-[y:HAS_RELATIONSHIPS]->(d) 
         SET y.DATABASE = 'Yes'", $serverToDbArray);
+    $stack->push("CREATE INDEX ON :Database(name, surname)");
     $resultMapping = $result->getResult();
     foreach ($resultMapping as $eachMapping) {
         //Initialize all variable from mapping result
@@ -90,11 +119,14 @@ foreach ($extractResult as $result) {
         ];
 
         $dbToUserArray = array_merge($dbUserArray, $databaseArray);
-        //Create Database to User relationships
+        /**
+         *  Create Database to User relationship with index on user of surname and name
+         */
         $stack->push("MERGE (d:Database {name: {databaseName}, surname: {databaseSurname}}) 
             MERGE (u:User {name: {databaseUserName}, type: {userType}, surname: {databaseUserSurname}}) 
             MERGE (d)<-[y:HAS_RELATIONSHIPS]-(u)
             SET y.USER_OF = 'Yes'", $dbToUserArray);
+        $stack->push("CREATE INDEX ON :User(name, surname)");
         // if ($loginName !== null) {
         //     $loginArray = [
         //         'loginName' => $loginName
@@ -110,11 +142,14 @@ foreach ($extractResult as $result) {
                 'roleSurname' => "{$databasePrefix}{$role}"
             ];
             $roleToUserArray = array_merge($roleArray, $dbUserArray);
-            //Create Role to User relationships
+            /**
+             * Create Role to User Relationships with index on Role of surname and name
+             */
             $stack->push("MERGE (r:Role {name: {role}, surname: {roleSurname}}) 
                 MERGE (u:User {name: {databaseUserName}, type: {userType}, surname: {databaseUserSurname}}) 
                 MERGE (r)<-[y:HAS_RELATIONSHIPS]-(u)
                 SET y.MEMBER_OF = 'Yes'", $roleToUserArray);
+            $stack->push("CREATE INDEX ON :Role(name, surname)");
         }
         $schemaPrefix = "{$databasePrefix}{$schema}.";
         if ($schema !== null) {
@@ -123,18 +158,24 @@ foreach ($extractResult as $result) {
                 'schemaSurname' => rtrim($schemaPrefix, ".")
             ];
             $schemaToDbArray = array_merge($schemaArray, $databaseArray);
+            /**
+             * Create Schema to Database Relationships with index on Schema of surname and name
+             */
             $stack->push("MERGE (o:Database {name: {databaseName}, surname:{databaseSurname}}) 
                 MERGE (sc:Schema {name: {schema}, surname:{schemaSurname}}) 
                 MERGE (o)-[y:HAS_RELATIONSHIPS]->(sc)
                 SET y.SCHEMA = 'Yes'", $schemaToDbArray);
+            $stack->push("CREATE INDEX ON :Schema(name, surname)");
             // $stack->push("MERGE (o:Object {name: {objectName}, type: {objectType}}) 
             //     MERGE (sc:Schema {name: {schema}}) 
             //     MERGE (o)<-[y:HAS_RELATIONSHIPS]-(sc)
             //     SET y.OWNS = 'Yes'", $schemaToObjArray);
         }
         if ($permissionType !== null) {
-            //Create User to Object relationships
             $permissionState = $permissionState === "GRANT" ? "Yes": "No";
+            /**
+             * Default Query for User to Object Relationships
+             */
             $query = "MERGE (u:User {name: {databaseUserName}, type: {userType}, surname: {databaseUserSurname}}) 
                 MERGE (o:Object {name: {objectName}, type: {objectType}, surname:{objectSurname}}) 
                 MERGE (u)-[p:HAS_RELATIONSHIPS]->(o)
@@ -146,23 +187,29 @@ foreach ($extractResult as $result) {
                     'objectType' => $objectType,
                     'objectSurname' => rtrim($objectPrefix, ".")
                 ];
-                //Create Object to Schema Relationships
+                /**
+                 * Create Object to Schema Relationships with index on Object of name and surname
+                 */
                 $schemaToObjArray = array_merge($schemaArray, $objectArray);
                 $stack->push("MERGE (o:Object {name: {objectName}, type: {objectType}, surname:{objectSurname}}) 
                     MERGE (sc:Schema {name: {schema}, surname:{schemaSurname}}) 
                     MERGE (o)<-[y:HAS_RELATIONSHIPS]-(sc)
                     SET y.OWNS = 'Yes'", $schemaToObjArray);
+                $stack->push("CREATE INDEX ON :Object(name, surname)");
                 if ($columnName !== null) {
                     $columnArray = [
                         'columnName' => $columnName,
                         'columnSurname' => "{$objectPrefix}{$columnName}"
                     ];
                     $objToColumnArray = array_merge($objectArray, $columnArray);
-                    //Create Object to Column Relationships
+                    /**
+                     * Create Object to Column Relationships with index on name and surname
+                     */
                     $stack->push("MERGE (o:Object {name: {objectName}, type: {objectType}, surname:{objectSurname}}) 
                         MERGE (c:Column {name: {columnName}, surname:{columnSurname}}) 
                         MERGE (o)-[y:HAS_RELATIONSHIPS]->(c)
                         SET y.COLUMN = 'Yes'", $objToColumnArray);
+                    $stack->push("CREATE INDEX ON :Column(name, surname)");
                 }
             } else {
                 switch ($objectType) {
@@ -172,7 +219,9 @@ foreach ($extractResult as $result) {
                             'objectType' => $objectType,
                             'objectSurname' => rtrim($databasePrefix, ".")
                         ];
-                        //Create User to Database Relationships
+                        /**
+                         * User to Databse Relationships Query
+                         */
                         $query = "MERGE (u:User {name: {databaseUserName}, type: {userType}, surname: {databaseUserSurname}}) 
                             MERGE (o:Database {name: {objectName}, surname:{objectSurname}}) 
                             MERGE (u)-[p:HAS_RELATIONSHIPS]->(o)
@@ -185,9 +234,12 @@ foreach ($extractResult as $result) {
                         ];
                 }
             }
-            //Create Object to User Relationship
+            /**
+             * Create User to Object Relationships with index on Object of surname and name
+             */
             $userToPermToObjArray = array_merge($dbUserArray, $objectArray);
             $stack->push($query, $userToPermToObjArray);
+            $stack->push("CREATE INDEX ON :Object(name, surname)");
         }
     }
 }
