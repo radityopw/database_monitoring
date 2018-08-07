40373c9e5740164e9c1861dd8e51f6621f0bdf79
Fairy Hunter <fairyhunter13@gmail.com>
2018-08-07 21:20:37 +0700

    Progress - Modifying docker compose :whale: - Modifying example configuration for supervisor :sparkles: - Finishing supervisord-cron-debian for supervisor and cron :whale: - Adding cronjob config :wrench:

--- app/Components/user_extract.php
+++ app/Components/user_extract.php
@@ -4,32 +4,32 @@ namespace Dependency\Components;
 
 // $startExtract = microtime(true);
 use Dependency\Database\SqlConnectionMapper;
-use PDO;
+// use PDO;
 
 /**
  * Init SQL Server Connection
  */
-$databaseConfig = require config_path('database.php');
-$sqlSrvConfig = $databaseConfig['connections']['sqlsrv'];
-$sqlsrv = createSqlServerConnection($sqlSrvConfig['host'], $sqlSrvConfig['port'], $sqlSrvConfig['username'], $sqlSrvConfig['password']);
+// $databaseConfig = require config_path('database.php');
+// $sqlSrvConfig = $databaseConfig['connections']['sqlsrv'];
+// $sqlsrv = createSqlServerConnection($sqlSrvConfig['host'], $sqlSrvConfig['port'], $sqlSrvConfig['username'], $sqlSrvConfig['password']);
 
 /**
  * Query to extract database list from SQL Server
  */
-$queryConfig = require config_path('query.php');
-$query = $queryConfig['sqlserver']['extract_database'];
+// $queryConfig = require config_path('query.php');
+// $query = $queryConfig['sqlserver']['extract_database'];
 
 /**
  * Mapping the query result to SqlConnectionMapper Class
  */
-$result = tap($sqlsrv->prepare($query))->execute()->fetchAll(PDO::FETCH_OBJ);
-$resultColl = collect($result)->pluck('name')->mapInto(SqlConnectionMapper::class);
+// $result = tap($sqlsrv->prepare($query))->execute()->fetchAll(PDO::FETCH_OBJ);
+// $resultColl = collect($result)->pluck('name')->mapInto(SqlConnectionMapper::class);
 // dd($resultColl);
 
 /**
  * Testing code for presentation in final project
  */
-// $resultColl = collect("resits")->mapInto(SqlConnectionMapper::class);
+$resultColl = collect("resits")->mapInto(SqlConnectionMapper::class);
 // $resultColl = collect("AdventureWorks2016_EXT")->mapInto(SqlConnectionMapper::class);
 // dd("This is the result of mapping query result to result object:", $resultColl);
 
